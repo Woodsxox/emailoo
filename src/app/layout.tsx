dd91a51fa2b3bb407c8cc9898eb97e0ef43c0692
Seth <ofoma.s.woods@gmail.com>
2025-11-13 08:16:04 +0100

    first update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import { TRPCReactProvider } from "@/trpc/react";
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
